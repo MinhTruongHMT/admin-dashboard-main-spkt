042cd6e8f91564a88425df366360adaf5ccb37bf
Minh Truong <ldcc.truongtran@lotte.vn>
2024-04-26 11:59:07 +0700

    done notification

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -14,9 +14,9 @@ export default function DefaultLayout({
     <>
       <div className="flex flex-col overflow-hidden">
         <div className="mb-1 flex flex justify-center pl-8 pr-8">
-          {/* <div>
+          <div>
             <Image src="/Banner.png" alt="Image" width={900} height={200} />
-          </div> */}
+          </div>
         </div>
         <div className="flex h-screen">
           <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
@@ -24,6 +24,10 @@ export default function DefaultLayout({
             {/* <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
             <main>
               <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                <div className="text-title-md2 font-semibold text-black dark:text-white"></div>
+                <h1 className="text-center text-title-md2 font-semibold text-black dark:text-white bg-[#D9D9D9] p-5 mb-3">
+                  GIÁM SÁT, ĐIỀU KHIỂN HỆ THỐNG CHIẾU SÁNG
+                </h1>
                 {children}
               </div>
             </main>
