b3c4e45dac7859901a37b886ebb791834c55b28e
Minh Truong <ldcc.truongtran@lotte.vn>
2024-06-03 22:24:43 +0700

    add imagee

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,17 +1,59 @@
+"use client";
+import { database } from "@/configs/filebaseConfig";
+import { get, onValue, ref } from "firebase/database";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function UserInfo() {
+  const [bright, setBright] = useState<string>("");
+
+  useEffect(() => {
+    const usersRef = ref(database, "Monitor");
+    get(usersRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const userArray = Object.entries(snapshot.val()).map(
+            ([id, data]: any) => ({
+              id,
+              ...data,
+            }),
+          );
+
+          const brightData = userArray.find((e) => e.id == "Relay Output 2");
+          setBright(brightData.data);
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    onValue(usersRef, (snapshot) => {
+      if (snapshot.exists()) {
+        const userArray = Object.entries(snapshot.val()).map(
+          ([id, data]: any) => ({
+            id,
+            ...data,
+          }),
+        );
+
+        const brightData = userArray.find((e) => e.id == "Relay Output 2");
+        setBright(brightData.data);
+      }
+    });
+  }, []);
   return (
-    <div className="text-center pt-5">
+    <div className="pt-5 text-center">
       <h1 className="font-medium text-black">ĐỒ ÁN TỐT NGHIỆP</h1>
       <h3>
-        <span className="font-medium text-black">TÊN ĐỀ TÀI:</span> Ứng dụng BMS trong điều khiển giám sát hệ thống chiếu sáng
-        (BMS điều khiển LED Driver, DALI)
+        <span className="font-medium text-black">TÊN ĐỀ TÀI:</span> Ứng dụng BMS
+        trong điều khiển giám sát hệ thống chiếu sáng (BMS điều khiển LED
+        Driver, DALI)
       </h3>
 
       <div className="flex justify-center">
-       
-          {/* <tbody>
+        {/* <tbody>
             <tr>
               <td  className="font-medium text-black text-left">GVHD</td>
               <td>TS. Lê Trọng Nghĩa</td>
@@ -29,10 +71,27 @@ export default function UserInfo() {
               <td>Sơn Linh Vủ 20142612</td>
             </tr>
           </tbody> */}
-           <div style={{display:'flex'}}>
-            <Image src="/school.jpg" alt="Image" width={900} height={200} style={{width:'500px'}} />
+        {bright == "1" ? (
+          <div style={{ display: "flex" }}>
+            <Image
+              src="/on.gif"
+              alt="Image"
+              width={900}
+              height={200}
+              style={{ width: "400px" }}
+            />
           </div>
-       
+        ) : (
+          <div style={{ display: "flex" }}>
+            <Image
+              src="/off.jpg"
+              alt="Image"
+              width={900}
+              height={200}
+              style={{ width: "400px" }}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
